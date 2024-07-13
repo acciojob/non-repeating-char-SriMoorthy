aa1af9371ad79e0c31e7ea5ce6ee98d7610ccc29
SriMoorthy <srimoorthy27@gmail.com>
2024-07-13 17:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let k = ''
+	let flag = false
+	for (let i = 0; i < str.length; i++) {
+		let count = 0
+		for (let j = 0; j < str.length; j++) {
+			if (str[i] == str[j]) {
+				 count += 1
+				 k = str[i]
+			}
+	   }
+		if(count>=2){
+			flag = true
+		}
+		if(count==1){
+			return k
+		}
+    }
+	if (flag==true) {
+		return 'null'
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
